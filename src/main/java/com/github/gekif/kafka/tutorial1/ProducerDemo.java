534abcd0aaee8402e25323b52743f3a9665e3241
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-09-25 17:02:47 +0700

    07. Kafka Java Programming (101): 4. Java Producer

--- src/main/java/com/github/gekif/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/gekif/kafka/tutorial1/ProducerDemo.java
@@ -1,8 +1,41 @@
 package com.github.gekif.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        String bootStrapServers = "127.0.0.1:9092";
+
+        // Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create The Producer
+        KafkaProducer<String, String> producer =
+                new KafkaProducer<String, String>(properties);
+
+        // Create a Producer Record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("first_topic", "hello world");
+
+        // Send Data - Asynchronous
+        producer.send(record);
+
+        // Flush Data
+        producer.flush();
+
+        // Flush and Close Producer
+        producer.close();
+
     }
 }
